6eb98e2240f91ea63ed6c65fc078b119f60f4593
varunmishrk99 <varun.mishra@ssipmt.com>
2022-02-19 19:37:13 +0530

    my change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-    return <div className="App">This is a test code</div>;
+    return <div className="App">This is a test code varun mishra</div>;
 }
 //this is comment
 export default App;
